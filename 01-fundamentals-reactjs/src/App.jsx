497aded751e2460cf7061c2e8b5ae7ea642e385f
Rafael Lindoso <rafael.lindoso@gmail.com>
2023-12-02 18:25:54 -0300

    Componente: Sidebar

--- 01-fundamentals-reactjs/src/App.jsx
+++ 01-fundamentals-reactjs/src/App.jsx
@@ -1,20 +1,31 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
-    <>
+    <div>
       <Header/>
-      <Post 
-        author="Melina" 
-        content="I can play the role of maiden. Turning rune fragments into strength. To aid you in your search for the Elden Ring. You need only take me with you. She is a mysterious young lady seen in an opening cutscene with Torrent, the Spirit Steed, and first met at the beginning of the Tarnished's journey. Melina wears a black robe and approaches players while they are resting at different Sites of Grace. She offers guidance throughout the game, grants the ability to level up and teleports you to certain key areas." 
-      />
-      <Post 
-        author="Ranni" 
-        content="I am the witch Ranni. I stole Death long ago, and search now for the dark path. Ranni is the daughter of Radagon and Rennala. After Radagon departed and became the consort to Marika, Ranni and her siblings were raised to demigod status, as they were now the children of the consort of a goddess. She appears to have some involvement in The Night of Black Knives, where Black Knife Assassins stole a shard of the Rune of Death and murdered Godwyn, the Golden. At the same time, Ranni seems to have lost her original body and now resides as spirit inhabiting an artificial doll body, seeking to restore her Empyrean status with the aid of blacksmith Iji, preceptor Seluvis, wolf warrior Blaidd, and the Tarnished." 
-      />
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Melina" 
+            content="I can play the role of maiden. Turning rune fragments into strength. To aid you in your search for the Elden Ring. You need only take me with you. She is a mysterious young lady seen in an opening cutscene with Torrent, the Spirit Steed, and first met at the beginning of the Tarnished's journey. Melina wears a black robe and approaches players while they are resting at different Sites of Grace. She offers guidance throughout the game, grants the ability to level up and teleports you to certain key areas." 
+          />
+          <Post 
+            author="Ranni" 
+            content="I am the witch Ranni. I stole Death long ago, and search now for the dark path. Ranni is the daughter of Radagon and Rennala. After Radagon departed and became the consort to Marika, Ranni and her siblings were raised to demigod status, as they were now the children of the consort of a goddess. She appears to have some involvement in The Night of Black Knives, where Black Knife Assassins stole a shard of the Rune of Death and murdered Godwyn, the Golden. At the same time, Ranni seems to have lost her original body and now resides as spirit inhabiting an artificial doll body, seeking to restore her Empyrean status with the aid of blacksmith Iji, preceptor Seluvis, wolf warrior Blaidd, and the Tarnished." 
+          />
+        </main>
+      </div>
+
+
+      
+    </div>
   )
 }
