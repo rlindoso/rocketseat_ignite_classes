e985b2f4aa582ed142e24384cb54a3863ee4252f
Rafael Lindoso <rafael.lindoso@gmail.com>
2023-12-02 18:52:58 -0300

    Finalizando Sidebar

--- 01-fundamentals-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentals-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,18 @@ export function Sidebar() {
             />
 
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://images.unsplash.com/photo-1618344780695-7d54031f140d?q=50&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+                />
+
                 <strong>Melina</strong>
                 <span>undefined dog breed</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Edit your profile
                 </a>
             </footer>
