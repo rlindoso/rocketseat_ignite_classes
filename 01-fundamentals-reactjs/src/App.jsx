57f87d5d99b7a481e39acab92ff7936ee8c383f9
Rafael Lindoso <rafael.lindoso@gmail.com>
2023-12-02 16:15:44 -0300

    Componentes

--- 01-fundamentals-reactjs/src/App.jsx
+++ 01-fundamentals-reactjs/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
+import { Post } from './Post'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Post/>
+      <Post/>
+      <Post/>
     </>
   )
 }
-
-export default App
