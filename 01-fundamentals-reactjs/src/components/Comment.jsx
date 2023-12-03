8dbc46793d0a3c90315902c56554106e601ce57f
Rafael Lindoso <rafael.lindoso@gmail.com>
2023-12-03 00:53:44 -0300

    Componente: Comment

---
+++ 01-fundamentals-reactjs/src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/rlindoso.png" alt='' />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Rafael Lindoso</strong>
+                            <time title='December 3rd at 12:54 pm' dateTime='2023-12-03 12:54:47'>Around 2 hour ago</time>
+                        </div>
+                        
+                        <button title='Delete Comment'>
+                            <Trash />
+                        </button>
+                    </header>
+
+                    <p>Muito bem!!!</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Clap <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
